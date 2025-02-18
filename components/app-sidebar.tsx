8c96b474bcd73cc6e3e6b0808f3197ced2443a7a
Clinton's MiniPC <achuworifung@gmail.com>
2025-02-17 22:50:33 -0700

    completed part of the sidebar

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,70 @@
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarHeader,
+    SidebarFooter
+  } from "@/components/ui/sidebar"
+  import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+
+  // Menu items.
+const items = [
+    {
+      title: "Home",
+      url: "#",
+      icon: Home,
+    },
+    {
+      title: "Explore",
+      url: "/explore",
+      icon: Inbox,
+    },
+    {
+      title: "history",
+      url: "/history",
+      icon: Calendar,
+    },
+    {
+      title: "Search",
+      url: "#",
+      icon: Search,
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings,
+    },
+  ]
+  export function AppSidebar() {
+
+    
+    return (
+      <Sidebar>
+        <SidebarContent>
+            <SidebarGroup>
+                <SidebarGroupLabel>My Application</SidebarGroupLabel>
+                <SidebarGroupContent>
+                    <SidebarMenu>
+                        {items.map((item, index) => (
+                            <SidebarMenuItem key={index} >
+                                <SidebarMenuButton asChild>
+                                    <a href={item.url}>
+                                        <item.icon/>
+                                        <span>{item.title}</span>
+                                    </a>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        ))}
+                    </SidebarMenu>
+                </SidebarGroupContent>
+            </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+    )
+  }
+  
